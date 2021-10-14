b4c41cc74e1e05d3d2fb5dbb74f526fa84b379a5
arturisimo <arturisimo@gmail.com>
2021-10-14 17:30:55 +0200

    ioc: patron visitor

--- conecta4-mvc-ioc/src/main/java/org/cloud/apps/qa/view/util/Constraints.java
+++ conecta4-mvc-ioc/src/main/java/org/cloud/apps/qa/view/util/Constraints.java
@@ -4,6 +4,8 @@ import java.awt.GridBagConstraints;
 
 public class Constraints extends GridBagConstraints {
 	
+	private static final long serialVersionUID = -1642939696817765000L;
+
 	public Constraints(int gridX, int gridY, int gridWidth, int gridHeight) {
 		this.gridx = gridX;
 		this.gridy = gridY;
